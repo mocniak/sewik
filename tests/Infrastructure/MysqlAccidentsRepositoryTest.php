bd59b6cc8ea2d48609ffa5ca7f1e690aae18fd63
mocniak <karol@mocniak.pl>
2018-02-12 17:30:50 +0100

    fixed filter declaration in test

--- tests/Infrastructure/MysqlAccidentsRepositoryTest.php
+++ tests/Infrastructure/MysqlAccidentsRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Sewik\Infrastructure;
 
 use PHPUnit\Framework\TestCase;
 use Sewik\Domain\Accident;
+use Sewik\Domain\Filter;
 
 class MysqlAccidentsRepositoryTest extends TestCase
 {
@@ -20,5 +21,17 @@ class MysqlAccidentsRepositoryTest extends TestCase
         $id = 98080700;
         $accident = $this->repository->getAccident($id);
         $this->assertEquals($id, $accident->getId());
+    }
+
+    public function testRepositoryReturnsArrayOfAccidentsForGivenFilter()
+    {
+        $expectedCity = 'WARSZAWA';
+        /** @var Accident $accident */
+        $filter = new Filter([Filter::COLUMN_LOCALITY . ' = \'' . $expectedCity . '\'']);
+        $accidents = $this->repository->findFilteredAccidents($filter);
+        $this->assertEquals(50, count($accidents));
+        foreach ($accidents as $accident) {
+            $this->assertEquals($expectedCity, $accident->getLocality());
+        }
     }
 }
