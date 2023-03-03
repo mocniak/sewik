877b77fc12499d83272876ad6188a5c802dedc26
mocniak <karol@mocniak.pl>
2023-03-03 18:11:46 +0100

    use pdo instead of mysqli

--- src/Infrastructure/MysqlDatabase.php
+++ src/Infrastructure/MysqlDatabase.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Sewik\Infrastructure;
 
 use Sewik\Domain\DatabaseInterface;
@@ -8,35 +9,27 @@ use Sewik\Domain\QueryResult;
 
 class MysqlDatabase implements DatabaseInterface
 {
-    private $link;
+    private \PDO $link;
 
-    public function __construct(string $host, string $user, string $password, string $database)
+    public function __construct(string $dsn)
     {
-        $this->link = new \mysqli($host, $user, $password, $database);
-        $this->link->set_charset('utf8');
-        if ($this->link->connect_errno) {
-            throw new \RuntimeException("Connect failed: %s\n", $this->link->connect_error);
+        try {
+            $this->link = new \PDO($dsn, 'root','very_secure89');
+        } catch (\Throwable $e) {
+            throw new \RuntimeException("Connection failed: %s\n", 0, $e);
         }
     }
 
     public function executeQuery(Query $query): QueryResult
     {
         $time = microtime(true);
-        $resultQuery = $this->link->query($query->getSqlQuery());
-        if (!$resultQuery) throw new InvalidQueryException('Query Failed: ' . $this->link->error . '. Query: ' . $query->getSqlQuery());
-        $result = $resultQuery->fetch_all(MYSQLI_ASSOC);
-        $fields = $resultQuery->fetch_fields();
-        $headers = [];
-        foreach ($fields as $field) {
-            $headers[] = $field->name;
+        try {
+            $statement = $this->link->query($query->getSqlQuery());
+        } catch (\Throwable $throwable) {
+            throw new InvalidQueryException('Query Failed: ' . $throwable->getMessage() . '. Query: ' . $query->getSqlQuery(), 0, $throwable);
         }
-
-        $resultQuery->close();
-        return new QueryResult($result, $headers, microtime(true) - $time);
-    }
-
-    public function __destruct()
-    {
-        $this->link->close();
+        $result = $statement->fetchAll();
+        var_dump($result);
+        return new QueryResult($result, $result, microtime(true) - $time);
     }
 }
