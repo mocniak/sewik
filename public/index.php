ade75bb92805451ed194dacc975a5e9f26e98a32
mocniak <karol@mocniak.pl>
2023-03-08 11:30:35 +0100

    temporary closed

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
-use Sewik\Kernel;
+//use Sewik\Kernel;
+//
+//require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+//
+//return function (array $context) {
+//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+//};
+?>
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+<h1>Prace serwisowe</h1>
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+<p>Planowany koniec: 9 marca 2023</p>
